f3008cc12ffc73d88d19ec5cb7c3a12afb607161
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-02-16 22:19:35 -0500

    c

---
+++ src/assimp_stubs.cpp
@@ -0,0 +1,32 @@
+// Nothing
+//because i have the linker 🥀
+// so fuck this file its a placeholder
+// coversation with my self{
+// who the fuck is that? 
+// You dont want too know who that is.
+// oh i wanna know who that is mahdi show me!
+// *sigh*
+// assimp stubs.
+// "Assimp stubs?"
+// who the fuck is assimp stubs?!
+// ive never heard of assimp stubs have you ever used it?
+// no
+// nobody uses a stub and keeps it
+// was there such a demand of fucking placeholders
+// that you use it
+// thats a coversation between devs no one ever had
+// hey bro whats your favorite place holder
+// oh you know its stub
+// oh mine too
+// everyone loves stub
+// wanna make out 
+// sure
+// *imitation gay noises*
+// i sure wish you were stub
+// because aparently its a fan fucking favorite
+// look compiler just calm down
+// dont tell me to calm the fuck down when i do shit and theres no c-make but aparantly a stub exists
+// look msvc you fucking break when it doesnt exist
+// what the absolute FUCK
+// }
+// thank you for watching my shity diag
