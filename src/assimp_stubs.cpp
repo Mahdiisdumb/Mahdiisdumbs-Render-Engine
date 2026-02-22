8f9ca26ca985b6bbfc0719bb475ae0035d61560e
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-02-21 21:42:35 -0500

    Update assimp_stubs.cpp

--- src/assimp_stubs.cpp
+++ src/assimp_stubs.cpp
@@ -1,32 +1,42 @@
-// Nothing
-//because i have the linker 🥀
-// so fuck this file its a placeholder
-// coversation with my self{
-// who the fuck is that? 
-// You dont want too know who that is.
-// oh i wanna know who that is mahdi show me!
-// *sigh*
-// assimp stubs.
-// "Assimp stubs?"
-// who the fuck is assimp stubs?!
-// ive never heard of assimp stubs have you ever used it?
-// no
-// nobody uses a stub and keeps it
-// was there such a demand of fucking placeholders
-// that you use it
-// thats a coversation between devs no one ever had
-// hey bro whats your favorite place holder
-// oh you know its stub
-// oh mine too
-// everyone loves stub
-// wanna make out 
-// sure
-// *imitation gay noises*
-// i sure wish you were stub
-// because aparently its a fan fucking favorite
-// look compiler just calm down
-// dont tell me to calm the fuck down when i do shit and theres no c-make but aparantly a stub exists
-// look msvc you fucking break when it doesnt exist
-// what the absolute FUCK
-// }
-// thank you for watching my shity diag
+/*
+Nothing
+because I have the linker 🥀
+so fuck this file, it's a placeholder
+
+conversation with myself {
+  (MSVC) So have I, Mahdi, I've been here for years. Who the FUCK is that?!
+  (Me) You don't know who that is.
+  (MSVC) Oh, I wanna know who that is, Mahdi! Show me!
+  (Me) *sigh*
+  (Me) Assimp stubs.
+  (MSVC) "Assimp stubs?"
+  (MSVC) Who the FUCK is Assimp stubs?!
+  (MSVC) I've never heard of Assimp stubs. Have you used it?
+  (Me) No.
+  (MSVC) Nobody uses a stub and keeps it.
+  (MSVC) Was there such a demand that they put Stubs in the FUCKING Render Engine?!
+  (MSVC) That's a conversation no one's ever had.
+  (MSVC) Hey bro, what's your favorite placeholder?
+  (MSVC) Oh, you know, it's Stub.
+  (MSVC) Oh, mine too. Everybody loves Stub.
+  (MSVC) Wanna make out?
+  (MSVC) Sure.
+  (MSVC) *imitation gay noises*
+  (MSVC) I sure wish you were Stub, because apparently he's a fan FUCKING favorite.
+  (Me) Look, compiler, just calm down.
+  (MSVC) Don't tell me to calm down when I do shit and there's no CMake, but apparently a Stub exists!
+  (Me) Look, MSVC, you FUCKING break when it doesn't exist.
+  (MSVC) He should be the next face of your projects! Master Stub! Stub Bait! Stub FUCK!
+  (MSVC) Oh Mahdi, look, look, Stub...
+  (MSVC) You know he's kind of cool. I guess kind of cool.
+  (Me) What does he do?
+  (MSVC) I do compiling and I can build!
+  (MSVC) What the FUCK does he do?!
+  (Me) Nothing.
+  (MSVC)Nothing. That's exactly right, Mahdi. He's not special at all.
+  (MSVC) He's just some make-a-wish kid's character that they put in the Render Engine.
+  (MSVC) Hey little Billy, congratulations. I know you're gonna die in a week, but that's okay, because that crayon drawing you did? They put it in the Render Engine and they named it FUCKING Stub.
+}
+
+Thank you for watching my shitty diag.
+*/
